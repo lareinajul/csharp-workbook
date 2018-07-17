c299eba1a0f9b60bd3b21b976da12fe973be2afb
Julie Adams <lareinajul@htmail>
2018-07-16 21:06:57 -0500

    Hello World Changes

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿
+using System;
 
-namespace HelloWorld
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+       string name = "";
+		int age = 0;
+		int year = 0;
+		
+		Console.WriteLine("Please enter your name: ");
+		name = Console.ReadLine();
+		Console.WriteLine("Please enter your age: ");
+		age = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Please enter the year: ");
+		year = Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("Hello! My name is {0} and I am {1} years old. I was born in {2}.", name, age, year-age);
     }
 }
