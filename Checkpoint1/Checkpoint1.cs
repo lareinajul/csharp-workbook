f44520fe1b100da225701decbe489c09059cb7cd
Julie Adams <lareinajul@hotmail.com>
2018-08-01 20:18:54 -0500

    Working on Checkpoint1

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -4,9 +4,41 @@ namespace Checkpoint1
 {
     class Program
     {
+        int count = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+
+        void NumberCheck()
+        {
+            for(var i=1; i<101;i++)
+            {
+                var r = i%3;
+                var checkResult=RemainderCheck(r);
+
+                if(checkResult)
+                {
+                    count = count++;
+                }
+               
+               Console.WriteLine(count);
+
+            }
+        }
+
+
+        bool RemainderCheck(int  r)
+        {
+            //return r==0 ? true : false;
+            if (r==0)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
     }
 }
