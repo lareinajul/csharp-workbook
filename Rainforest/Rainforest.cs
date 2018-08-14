c0aa3414d03eac0197a9b77ab0578aa448d96567
Julie Adams <lareinajul@hotmail.com>
2018-08-13 21:05:28 -0500

    working on Rainforest

--- Rainforest/Rainforest.cs
+++ Rainforest/Rainforest.cs
@@ -6,7 +6,13 @@ namespace Rainforest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create your Company, Warehouse, Container, Item classes using the field names given. For your fields that hold other objects (eg. Company.warehouses, Warehouse.containers, Container.items), use Lists. Be sure to not only define your fields, but also to set them in your constructor functions.
+            // In your Program.Main function, instantiate a new company called Rainforest.
+            // Build some Warehouses in a few different cities. Add those warehouses to your company.
+            // Build some containers for each warehouse. You can name them City-Number (eg, Austin-0).
+            // Create some items, such as groceries (eg, bananas, etc), add those into each container.
+            // Create a manifest that lists your company, the location of each warehouse, the containers in each warehouse, and the products in each container.
+            // Build an "index" of the items, so if a customer asks for an item, you can tell the worker where to go get it. A Dictionary is great for this.
         }
     }
 }
