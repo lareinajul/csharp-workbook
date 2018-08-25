6f7d4a0bbf29990548c9e5f7cdb797f958711400
Julie Adams <lareinajul@hotmail.com>
2018-08-25 11:19:12 -0500

    Working on EnumPractice

---
+++ EnumPractice/EnumPractice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()   
+	{		
+        int myDay = 15;
+        int myMonth = 10;
+                
+		Console.WriteLine("Please input a year");
+		int myYear = Convert.ToInt32(Console.ReadLine());
+
+        DateTime dt = new DateTime(myYear,myMonth, myDay);
+
+        int dow = (int)dt.DayOfWeek;
+
+        var dowName = (DaysOfTheWeekWithNumbers)dow;
+        Console.WriteLine(dowName);
+
+	}	
+	enum DaysOfTheWeekWithNumbers
+	{
+		Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 0
+	}
+}
